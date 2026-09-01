b21d6810fdce79fc19a533c8435eddb8a16c3252
Vighanesh Gavit <gavitvs0412@gmail.com>
2026-09-01 16:34:01 +0530

    Feature: Added Crossref Source Connector

--- src/researchflow/sources/__init__.py
+++ src/researchflow/sources/__init__.py
@@ -1,7 +1,9 @@
 from .base import PaperSource
+from .crossref import CrossrefSource
 from .openalex import OpenAlexSource
 
 __all__ = [
     "PaperSource",
+    "CrossrefSource",
     "OpenAlexSource",
 ]
